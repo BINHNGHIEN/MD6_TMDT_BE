468bb7ec872676c4a1cf523566e1ab09034c0cc0
BINHNGHIEN <haibinh.94.bui@gmail.com>
2022-07-18 08:56:24 +0700

    start

---
+++ src/main/java/com/example/md6_final_project_be/Md6FinalProjectBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.md6_final_project_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Md6FinalProjectBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Md6FinalProjectBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/md6_final_project_be/Md6FinalProjectBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.md6_final_project_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Md6FinalProjectBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
