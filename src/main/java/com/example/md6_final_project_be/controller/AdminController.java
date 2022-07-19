84d7f98992c19865b86c8c5e3af06bf28a0275ce
unknown <manh87835@gmail.com>
2022-07-19 09:53:51 +0700

    Update constructer of AppUser Class

--- src/main/java/com/example/md6_final_project_be/controller/AdminController.java
+++ src/main/java/com/example/md6_final_project_be/controller/AdminController.java
@@ -1,6 +1,7 @@
 package com.example.md6_final_project_be.controller;
 import com.example.md6_final_project_be.dto.response.ResponseMessage;
 import com.example.md6_final_project_be.model.AppUser;
+import com.example.md6_final_project_be.model.Role;
 import com.example.md6_final_project_be.security.jwt.JwtProvider;
 import com.example.md6_final_project_be.security.jwt.JwtTokenFilter;
 import com.example.md6_final_project_be.service.admin.IAdminService;
@@ -13,7 +14,9 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 @RestController
 @RequestMapping("/admin")
@@ -37,9 +40,19 @@ public class AdminController {
   @GetMapping("/search/{name}")
     public ResponseEntity<?> findUserByName(@PathVariable String name){
       List<AppUser> appUserList= (List<AppUser>) adminServiceIMPL.findAppUserByNameContaining(name);
+      List<AppUser> appUserList1=new ArrayList<>();
+      for (int i = 0; i < appUserList.size(); i++) {
+         Set<Role> roles =appUserList.get(i).getRoles();
+         if (roles.contains("user")){
+             appUserList1.add(appUserList.get(i));
+         }
+      }
+
+
+
       if (appUserList.isEmpty()){
           return new ResponseEntity<>(new ResponseMessage("user_not_found"),HttpStatus.NOT_FOUND);
       }
-      return new ResponseEntity<>(appUserList, HttpStatus.OK);
-  }
+      return new ResponseEntity<>(appUserList, HttpStatus.OK);}
+
 }
