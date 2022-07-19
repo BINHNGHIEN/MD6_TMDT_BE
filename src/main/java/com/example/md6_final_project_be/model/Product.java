b469ac7b998baeeea73c4f259e468281bf92f8d0
hieupeo999 <88883566+hieupeo999@users.noreply.github.com>
2022-07-19 09:38:47 +0700

    fix bug class product

--- src/main/java/com/example/md6_final_project_be/model/Product.java
+++ src/main/java/com/example/md6_final_project_be/model/Product.java
@@ -30,4 +30,52 @@ public class Product {
         this.inventoryProduct = inventoryProduct;
         this.category = category;
     }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNameProduct() {
+        return nameProduct;
+    }
+
+    public void setNameProduct(String nameProduct) {
+        this.nameProduct = nameProduct;
+    }
+
+    public String getAvatarProduct() {
+        return avatarProduct;
+    }
+
+    public void setAvatarProduct(String avatarProduct) {
+        this.avatarProduct = avatarProduct;
+    }
+
+    public int getPriceProduct() {
+        return priceProduct;
+    }
+
+    public void setPriceProduct(int priceProduct) {
+        this.priceProduct = priceProduct;
+    }
+
+    public int getInventoryProduct() {
+        return inventoryProduct;
+    }
+
+    public void setInventoryProduct(int inventoryProduct) {
+        this.inventoryProduct = inventoryProduct;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
 }
