52e2e037012b9508099a2b3d39ed7b22f768272d
hieupeo999 <88883566+hieupeo999@users.noreply.github.com>
2022-07-19 14:24:37 +0700

    tao service product

--- src/main/java/com/example/md6_final_project_be/service/product/IProductService.java
+++ src/main/java/com/example/md6_final_project_be/service/product/IProductService.java
@@ -1,7 +1,12 @@
 package com.example.md6_final_project_be.service.product;
 
+import com.example.md6_final_project_be.model.Product;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public interface IProductService {
+
+
 }
--- src/main/java/com/example/md6_final_project_be/service/product/ProductServiceIMPL.java
+++ src/main/java/com/example/md6_final_project_be/service/product/ProductServiceIMPL.java
@@ -1,7 +1,17 @@
 package com.example.md6_final_project_be.service.product;
 
+import com.example.md6_final_project_be.model.Product;
+import com.example.md6_final_project_be.repository.IProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class ProductServiceIMPL implements IProductService{
+    @Autowired
+    IProductRepository productRepository;
+    public Optional<Product> findByName(Product nameProduct){
+        return productRepository.findByName(nameProduct);
+    }
 }
