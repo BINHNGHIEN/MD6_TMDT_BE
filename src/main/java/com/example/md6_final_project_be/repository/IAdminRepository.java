38cc025adf90df44fdf4187e7e316bcdfe893509
unknown <manh87835@gmail.com>
2022-07-19 09:23:55 +0700

    admin search user  by name

--- src/main/java/com/example/md6_final_project_be/repository/IAdminRepository.java
+++ src/main/java/com/example/md6_final_project_be/repository/IAdminRepository.java
@@ -1,9 +1,13 @@
 package com.example.md6_final_project_be.repository;
 
-import com.example.md6_final_project_be.model.Admin;
+
+import com.example.md6_final_project_be.model.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface IAdminRepository extends JpaRepository<Admin,Long> {
+public interface IAdminRepository extends JpaRepository<AppUser,Long> {
+    Iterable<AppUser> findAppUserByNameContaining(String name);
 }
