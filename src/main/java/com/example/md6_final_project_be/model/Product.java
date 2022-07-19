5b4b18e55bfb453fd3da7e001eff21794c353f1e
hieupeo999 <88883566+hieupeo999@users.noreply.github.com>
2022-07-19 09:30:09 +0700

    create class product

--- src/main/java/com/example/md6_final_project_be/model/Product.java
+++ src/main/java/com/example/md6_final_project_be/model/Product.java
@@ -1,5 +1,33 @@
 package com.example.md6_final_project_be.model;
 
-public class Product {
+import javax.persistence.*;
 
+@Entity
+@Table(name = "products",uniqueConstraints ={
+        @UniqueConstraint(columnNames = {
+                "nameProduct"
+        })
+})
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String nameProduct;
+    private String avatarProduct;
+    private int priceProduct;
+    private int inventoryProduct;
+    @ManyToOne
+    Category category;
+
+    public Product() {
+    }
+
+    public Product(long id, String nameProduct, String avatarProduct, int priceProduct, int inventoryProduct, com.example.md6_final_project_be.model.Category category) {
+        this.id = id;
+        this.nameProduct = nameProduct;
+        this.avatarProduct = avatarProduct;
+        this.priceProduct = priceProduct;
+        this.inventoryProduct = inventoryProduct;
+        this.category = category;
+    }
 }
